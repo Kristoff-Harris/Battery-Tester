c3c87ccccca347e817775a378fd8b5b2af7011d0
Cray Harris <cjh6470@rit.edu>
2017-09-07 22:27:46 -0400

    Tkinter Wireframe of battery testing GUI

--- main.py
+++ main.py
@@ -1,24 +1,95 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+def print_selected(): {
+    print("Combobox changed")
+}
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+root = Tk()
+root.title("Battery Testing Application v0.1")
+content = ttk.Frame(root)
 
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+
+
+mode = StringVar()
+simul_option = ttk.Radiobutton(content, text='Test Banks Simul', variable=mode, value='simul')
+individ_option = ttk.Radiobutton(content, text='Test Banks Indiv', variable=mode, value='individual')
+
+run_param = StringVar()
+run_val = ttk.Entry(content)
+volt_option = ttk.Radiobutton(content, text='V Input', variable=run_param, value='v_selected')
+current_option = ttk.Radiobutton(content, text='C Input', variable=run_param, value='c_selected')
+power_option = ttk.Radiobutton(content, text='Pow Input', variable=run_param, value='pow_selected')
+static_option = ttk.Radiobutton(content, text='Use PreProg', variable=run_param, value='preprog_selected')
+
+
+countryvar = StringVar()
+country = ttk.Combobox(content, textvariable=countryvar)
+country.bind('<<ComboboxSelected>>', print_selected())
+country['values'] = ('Pre-defined 1', 'Pre-defined 2', 'Pre-defined 3')
+
+namelbl = ttk.Label(content, text="Name")
+name = ttk.Entry(content)
+
+contact_info = ttk.Label(content, text="Built by Dan Harris - dan.harris@gmail.com")
+
+testing_status = ttk.Label(content, text="TESTING STATUS: INACTIVE ")
+
+bank_1_current_output_label = ttk.Label(content, text="B1 Curr: 5")
+bank_1_volt_output_label = ttk.Label(content, text="B1 V: 13")
+bank_1_load_output_label = ttk.Label(content, text="B1 Load: 10")
+bank_1_heartbeat = ttk.Label(content, text="B1 Status: Online")
+
+bank_2_current_output_label = ttk.Label(content, text="B2 Curr: 12")
+bank_2_volt_output_label = ttk.Label(content, text="B2 V: 50")
+bank_2_load_output_label = ttk.Label(content, text="B2 Load: 100")
+bank_2_heartbeat = ttk.Label(content, text="B2 Status: Online")
+
+onevar = BooleanVar()
+twovar = BooleanVar()
+threevar = BooleanVar()
+onevar.set(True)
+twovar.set(False)
+threevar.set(True)
+
+#one = ttk.Checkbutton(content, text="One", variable=onevar, onvalue=True)
+#two = ttk.Checkbutton(content, text="Two", variable=twovar, onvalue=True)
+#hree = ttk.Checkbutton(content, text="Three", variable=threevar, onvalue=True)
+ok = ttk.Button(content, text="Begin Testing")
+cancel = ttk.Button(content, text="Stop")
+
+
+testing_status.grid(column=7, row=0)
+simul_option.grid(column=0, row=1)
+individ_option.grid(column=0, row=2)
+
+bank_1_current_output_label.grid(column=2, row=1)
+bank_1_volt_output_label.grid(column=2, row=2)
+bank_1_load_output_label.grid(column=2, row=3)
+bank_1_heartbeat.grid(column=7, row=1)
+
+bank_2_current_output_label.grid(column=4, row=1)
+bank_2_volt_output_label.grid(column=4, row=2)
+bank_2_load_output_label.grid(column=4, row=3)
+bank_2_heartbeat.grid(column=7, row=2)
+
+
+run_val.grid(column=6, row=4)
+volt_option.grid(column=4, row=4)
+current_option.grid(column=4, row=5)
+power_option.grid(column=4, row=6)
+static_option.grid(column=4, row=7)
+
+ok.grid(column=7, row=5)
+cancel.grid(column=7, row=6)
+#namelbl.grid(column=5, row=0, columnspan=2)
+#name.grid(column=5, row=1, columnspan=2)
+
+content.grid(column=0, row=0)
+#frame.grid(column=0, row=0, columnspan=3, rowspan=2)
+country.grid(column=5, row=7, columnspan=2)
+contact_info.grid(column=6, row=8, columnspan=3)
+
+root.mainloop()
