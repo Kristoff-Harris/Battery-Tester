62d118332722f0021c31aba79764707557c0071d
Chris Harris <cjh6470@rit.edu>
2017-09-10 13:59:25 -0700

    Create deviceconnection.py
    Adding the deviceconnection file which will house the code to read from the IEEE connection

---
+++ deviceconnection.py
@@ -0,0 +1,38 @@
+#-------------------------------------------------------------------------------------------------------------
+# File          : deviceconnection.py
+# Create Date   : 9/9/2017
+# Purpose       : Houses the code which connects to the bank IEEE
+#
+#-------------------------------------------------------------------------------------------------------------
+
+
+
+
+#  Ultimately your call but my thought is to return True if we can successfully connect to the Bank IEEE interface and
+# False if we cannot
+def getBank1ConnStatus():
+    #TODO  put code to do a healthcheck on Bank1
+    return True
+
+def getBank2ConnStatus():
+    # TODO  put code to do a healthcheck on Bank2
+    return True
+
+def getBank1Voltage():
+    return 4
+
+def getBank2Voltage():
+    return 6
+
+def getBank1Current():
+    return 7
+
+def getBank2Current():
+    return 8
+
+def getBank1Load():
+    return 10
+
+def getBank2Load():
+    return 11
+
