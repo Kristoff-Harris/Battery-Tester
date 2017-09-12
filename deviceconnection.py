c547653fa38641500c18b6a8ffa8a88dc6703ff4
dih112 <31753708+dih112@users.noreply.github.com>
2017-09-11 22:37:42 -0400

    Add files via upload
    First Communicating Version

--- deviceconnection.py
+++ deviceconnection.py
@@ -9,57 +9,86 @@
 #import [Library needed for IEEE if not serial]
 import serial
 import io
-
+import time
+ 
 #Static definition of COM Ports, maybe more elegant solution is possible in the future. 
-Bank1_Port='COM6'
+Bank1_Port='COM8' # This is the port of the WCL232 100-1000-12000
 Bank2_Port='COM7'
 
 #Define Serial Aliases 
 ser1 = serial.Serial()
 ser1.baudrate = 9600
 ser1.port = Bank1_Port
+ser1.timeout = 1
 
 
 
 ser2 = serial.Serial()
 ser2.baudrate = 9600
 ser2.port = Bank2_Port
+ser2.timeout = 1
 
-ser1.open()
-sio = io.TextIOWrapper(io.BufferedRWPair(ser1, ser1,1),encoding='ascii')
-sio.write(str("ID?\n"))
+sio1 = io.TextIOWrapper(io.BufferedRWPair(ser1, ser1,1),encoding='ascii')
 
-#sio.flush() # it is buffering. required to get the data out *now*
-#IDtype = sio.readline()
-ser1.close()
-IDtype2 = IDtype
+
 #  Ultimately your call but my thought is to return True if we can successfully connect to the Bank IEEE interface and
 # False if we cannot
 def getBank1ConnStatus():
     ser1.open()
-    ser1.write(b'ID?')
-    #Checkline = ser1.readline()
+    sio1.write(str("ID?\n"))
+    sio1.flush() # it is buffering. required to get the data out *now*
+    time.sleep(0.05)
+    Checkline = str(ser1.readline())
     ser1.close()
-    return True
+    if Checkline[2:-5] == 'WCL 100-1000-12000':
+        return True
+    else :
+        return False
 
 def getBank2ConnStatus():
     # TODO  put code to do a healthcheck on Bank2
-    return True
+    return False
 
 def getBank1Voltage():
-    return 4
+    ser1.open()
+    sio1.write(str("V?\n"))
+    sio1.flush() # it is buffering. required to get the data out *now*
+    time.sleep(0.05)
+    Checkline = str(ser1.readline())
+    ser1.close()
+   
+    lefttext=Checkline.partition(" v")[0]
+    return lefttext[2:]
+    
+    
 
 def getBank2Voltage():
     return 6
 
 def getBank1Current():
-    return 7
+    ser1.open()
+    sio1.write(str("I?\n"))
+    sio1.flush() # it is buffering. required to get the data out *now*
+    time.sleep(0.05)
+    Checkline = str(ser1.readline())
+    ser1.close()
+   
+    lefttext=Checkline.partition(" a")[0]
+    return lefttext[2:]
 
 def getBank2Current():
     return 8
 
 def getBank1Load():
-    return 10
+    ser1.open()
+    sio1.write(str("P?\n"))
+    sio1.flush() # it is buffering. required to get the data out *now*
+    time.sleep(0.05)
+    Checkline = str(ser1.readline())
+    ser1.close()
+   
+    lefttext=Checkline.partition(" w")[0]
+    return lefttext[2:]
 
 def getBank2Load():
     return 11
