5d1cea8cb7059786bd0c06abbee6ed5379e42dd4
Dan Harris <daniel.ian.harris@gmail.com>
2017-09-17 18:49:41 -0400

    Adding note about ranges

--- load_balance.py
+++ load_balance.py
@@ -2,3 +2,11 @@
 This function determines a mutiplier to apply to the load command to achieve the 
 desired current setpoint and adjusts the feedback to correct for calibration errors.
 """
+
+###Reminder, need to set the load bank range!
+"""
+RNG  x  Set the range (1-9) 
+ 
+(just a reminder)
+Range Voltage Current 1 HI HI 2 MED HI 3 LOW HI 4 HI MED 5 MED MED 6 LOW MED 7 HI LOW 8 MED LOW 9 LOW LOW 
+"""
